cf74dfcf301881c3d5019b86a6f74f5c505c712e
Sabeeh Mumtaz Alam <31985457+sabeehalam@users.noreply.github.com>
2023-02-16 19:28:19 +0500

    Pin changes for raspberry pi ethernet addition

--- ports/rp2/boards/PICO/mpconfigboard.h
+++ ports/rp2/boards/PICO/mpconfigboard.h
@@ -1,3 +1,17 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "Raspberry Pi Pico"
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (1408 * 1024)
+
+// Enable networking.
+#define MICROPY_PY_NETWORK                (1)
+
+// Wiznet HW config.
+#define MICROPY_HW_WIZNET_SPI_ID          (1)
+#define MICROPY_HW_WIZNET_SPI_BAUDRATE    (20 * 1000 * 1000)
+#define MICROPY_HW_WIZNET_SPI_SCK         (10)
+#define MICROPY_HW_WIZNET_SPI_MOSI        (11)
+#define MICROPY_HW_WIZNET_SPI_MISO        (12)
+#define MICROPY_HW_WIZNET_PIN_CS          (13)
+#define MICROPY_HW_WIZNET_PIN_RST         (15)
+// Connecting the INTN pin enables RECV interrupt handling of incoming data.
+#define MICROPY_HW_WIZNET_PIN_INTN        (21)
